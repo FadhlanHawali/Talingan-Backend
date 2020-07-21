39a88f393d70277247b764e663a7d0fc4dfc5a14
FadhlanHawali <fadhlan.hawali@mail.ugm.ac.id>
2020-07-21 10:31:56 +0700

    add wrapper

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+	"github.com/spf13/viper"
+
+	"github.com/Talingan-Backend/pkg/api"
 )
 
 func main() {
-	fmt.Printf("hello, world\n")
+
+	if err := api.VisionClassificationPredict(); err != nil {
+		fmt.Printf(err.Error())
+		return
+	}
+
+	router := mux.NewRouter()
+
+	handler := cors.AllowAll().Handler(router)
+	port := fmt.Sprintf(":%s", viper.Get("host.port"))
+	log.Printf("Server Running on port %s", port)
+	log.Fatal(http.ListenAndServe(port, handler))
+}
+
+func checkHealth() {
+
 }
