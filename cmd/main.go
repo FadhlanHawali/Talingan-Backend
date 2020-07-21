93656335b222c934e23a6ab93987bb17281d0d16
FadhlanHawali <fadhlan.hawali@mail.ugm.ac.id>
2020-07-21 10:48:03 +0700

    add default handler

--- cmd/main.go
+++ cmd/main.go
@@ -1,32 +1,36 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/Talingan-Backend/utils"
+
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
-	"github.com/spf13/viper"
-
-	"github.com/Talingan-Backend/pkg/api"
 )
 
 func main() {
 
-	if err := api.VisionClassificationPredict(); err != nil {
-		fmt.Printf(err.Error())
-		return
-	}
+	// viper.SetConfigFile("./config/dev.json")
+	// if err := viper.ReadInConfig(); err != nil {
+	// 	log.Fatalf("Error reading config file, %s", err)
+	// }
+
+	// if err := api.VisionClassificationPredict(); err != nil {
+	// 	fmt.Printf(err.Error())
+	// 	return
+	// }
 
 	router := mux.NewRouter()
-
+	router.HandleFunc("/", http.HandlerFunc(checkHealth))
 	handler := cors.AllowAll().Handler(router)
-	port := fmt.Sprintf(":%s", viper.Get("host.port"))
-	log.Printf("Server Running on port %s", port)
-	log.Fatal(http.ListenAndServe(port, handler))
+	// port := fmt.Sprintf(":%s", viper.Get("host.port"))
+	log.Printf("Server Running on port %s", "8080")
+	log.Fatal(http.ListenAndServe(":8080", handler))
 }
 
-func checkHealth() {
-
+func checkHealth(w http.ResponseWriter, r *http.Request) {
+	utils.WrapAPISuccess(w, r, "Success", 200)
+	return
 }
