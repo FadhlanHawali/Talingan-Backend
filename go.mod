a6d71ad7e82dde490df385a2e46c51231cece124
FadhlanHawali <fadhlan.hawali@mail.ugm.ac.id>
2020-07-22 13:08:16 +0700

    add mongodb

--- go.mod
+++ go.mod
@@ -7,5 +7,6 @@ require (
 	github.com/gorilla/mux v1.7.4
 	github.com/rs/cors v1.7.0
 	github.com/spf13/viper v1.7.0
+	go.mongodb.org/mongo-driver v1.3.5
 	google.golang.org/genproto v0.0.0-20200715011427-11fb19a81f2c
 )
--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"context"
+	"github.com/Talingan-Backend/database"
+	"github.com/spf13/viper"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"log"
 	"net/http"
 
@@ -11,25 +16,37 @@ import (
 
 func main() {
 
-	// viper.SetConfigFile("./config/dev.json")
-	// if err := viper.ReadInConfig(); err != nil {
-	// 	log.Fatalf("Error reading config file, %s", err)
-	// }
+	viper.SetConfigFile("./configs/config.yml")
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file, %s", err)
+	}
 
 	// if err := api.VisionClassificationPredict(); err != nil {
 	// 	fmt.Printf(err.Error())
 	// 	return
 	// }
 
+	db := database.DBInit("mongodb://127.0.0.1:27017")
+	inDB := &InDB{DB: db}
 	router := mux.NewRouter()
-	router.HandleFunc("/", http.HandlerFunc(checkHealth))
+	router.HandleFunc("/", http.HandlerFunc(inDB.checkHealth))
 	handler := cors.AllowAll().Handler(router)
 	// port := fmt.Sprintf(":%s", viper.Get("host.port"))
 	log.Printf("Server Running on port %s", "8080")
 	log.Fatal(http.ListenAndServe(":8080", handler))
 }
 
-func checkHealth(w http.ResponseWriter, r *http.Request) {
+func (idb *InDB) checkHealth(w http.ResponseWriter, r *http.Request) {
 	utils.WrapAPISuccess(w, r, "Success", 200)
+	err := idb.DB.Ping(context.Background(), readpref.Primary())
+	if err != nil{
+		log.Fatal(err)
+	}else{
+		log.Println("Connected !")
+	}
 	return
+}
+
+type InDB struct{
+	DB *mongo.Client
 }
